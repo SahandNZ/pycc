395e9f252c21e580649d9ccb4f8de692dd9d30f5
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2023-07-21 16:18:43 +0330

    Fill time frame values in its python file

--- pyccx/constant/time_frame.py
+++ pyccx/constant/time_frame.py
@@ -1,2 +1,15 @@
 class TimeFrame:
-    pass
+    MIN1 = 60
+    MIN5 = 5 * MIN1
+    MIN15 = 15 * MIN1
+    MIN30 = 30 * MIN1
+    HOUR1 = 60 * MIN1
+    HOUR2 = 2 * HOUR1
+    HOUR4 = 4 * HOUR1
+    HOUR6 = 6 * HOUR1
+    HOUR8 = 8 * HOUR1
+    HOUR12 = 12 * HOUR1
+    DAY1 = 24 * HOUR1
+    DAY3 = 3 * DAY1
+    WEEK1 = 7 * DAY1
+    MONTH1 = 30 * DAY1
