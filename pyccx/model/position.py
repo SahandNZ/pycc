25be79a7f945ba7fa03c76d6760436a585b95c2d
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2023-07-23 02:39:24 +0330

    Fix from_bitget method in position model

--- pyccx/model/position.py
+++ pyccx/model/position.py
@@ -18,9 +18,16 @@ class Position:
 
     @staticmethod
     def from_bitget(data: Dict):
-        long_volume = float(data[0]['available'])
-        short_volume = float(data[1]['available'])
-        data = data[0] if short_volume <= long_volume else data[1]
+        if 0 == len(data):
+            instance = Position()
+            instance.volume = 0
+            return instance
+        elif 1 == len(data):
+            data = data[0]
+        else:
+            long_volume = float(data[0]['available'])
+            short_volume = float(data[1]['available'])
+            data = data[0] if short_volume <= long_volume else data[1]
 
         instance = Position()
 
