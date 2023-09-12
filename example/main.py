946834ba707b2ce1fead060e34f7d48fe8e51cb4
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2023-09-12 05:49:31 +0330

    Add main.py as future status logger

---
+++ example/main.py
@@ -0,0 +1,40 @@
+import argparse
+import json
+import sys
+
+from pyccx.interface.exchange import Exchange
+
+
+def main():
+    sys.path.append('..')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config-path', action='store', type=str, required=False, default="config.json")
+    parser.add_argument('-e', '--exchange', action='store', type=str, required=False, default="binance")
+    parser.add_argument('-s', '--symbol', action='store', type=str, required=False, default='BTC-USDT')
+    args = parser.parse_args()
+
+    with open(args.config_path, 'r') as file:
+        data = json.load(file)
+
+    data = data[args.exchange]
+    exchange = Exchange.from_config(data)
+
+    # get open orders
+    orders = exchange.future.trade.get_open_orders(symbol=symbol)
+    print("\t\t- Get open orders")
+    print("\t\t\t- {:<32} {}".format("orders count", len(orders)))
+
+    # get position
+    position = exchange.future.trade.get_open_position(symbol=args.symbol)
+    print("\t\t- Get position")
+    print("\t\t\t- {:<32} {}".format("symbol", position.symbol))
+    print("\t\t\t- {:<32} {}".format("side", position.side))
+    print("\t\t\t- {:<32} {}".format("volume", position.volume))
+    print("\t\t\t- {:<32} {}".format("Leverage", position.leverage))
+    print("\t\t\t- {:<32} {}".format("Entry price", position.entry_price))
+    print("\t\t\t- {:<32} {}".format("Liquidation price", position.liquidation_price))
+
+
+if __name__ == '__main__':
+    main()
