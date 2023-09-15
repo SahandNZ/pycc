658497d2f7fa72686d2797e2e12c32870bfd6a18
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2023-09-15 18:30:29 +0330

    Add --close-position to main.py cli args

--- examples/main.py
+++ examples/main.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import os
 import sys
 from typing import List
 
@@ -7,7 +8,7 @@ from examples.future import future_examples
 from examples.spot import spot_examples
 from examples.wallet import wallet_examples
 from pyccx.constant.time_frame import TimeFrame
-from pyccx.data_collector import DataCollector
+from pyccx.data.local_data import LocalData
 from pyccx.interface.exchange import Exchange
 
 
@@ -23,9 +24,37 @@ def run_examples(exchange: Exchange, symbol: str, time_frame: TimeFrame):
     future_examples(exchange, symbol, time_frame)
 
 
-def download_candles(exchange: Exchange, symbols: List[str], time_frame: TimeFrame, data_root: str):
-    data_collector = DataCollector(exchange=exchange, market=exchange.future.market, data_root=data_root)
-    data_collector.download_symbols_candles(symbols=symbols, time_frame=time_frame)
+def download_candles(exchange: Exchange, symbols: List[str], time_frame: TimeFrame):
+    local_data = LocalData(exchange=exchange)
+    local_data.download_symbols_candles(symbols=symbols, time_frame=time_frame)
+
+
+def close_position(exchange: Exchange, symbol: str):
+    # get position to close position
+    position = exchange.future.trade.get_open_position(symbol=symbol)
+    print("\t\t- Get position")
+    print("\t\t\t- {:<32} {}".format("symbol", position.symbol))
+    print("\t\t\t- {:<32} {}".format("side", position.side))
+    print("\t\t\t- {:<32} {}".format("volume", position.volume))
+    print("\t\t\t- {:<32} {}".format("Leverage", position.leverage))
+    print("\t\t\t- {:<32} {}".format("Entry price", position.entry_price))
+    print("\t\t\t- {:<32} {}".format("Liquidation price", position.liquidation_price))
+
+    # post market order to close current position
+    reverse_side = -1 * position.side
+    order_id = exchange.future.trade.set_market_order(symbol=symbol, side=reverse_side, volume=position.volume)
+    print("\t\t- Post Market order")
+    print("\t\t\t- {:<32} {}".format("Buy market order", order_id))
+
+    # get position to check is position closed
+    position = exchange.future.trade.get_open_position(symbol=symbol)
+    print("\t\t- Get position")
+    print("\t\t\t- {:<32} {}".format("symbol", position.symbol))
+    print("\t\t\t- {:<32} {}".format("side", position.side))
+    print("\t\t\t- {:<32} {}".format("volume", position.volume))
+    print("\t\t\t- {:<32} {}".format("Leverage", position.leverage))
+    print("\t\t\t- {:<32} {}".format("Entry price", position.entry_price))
+    print("\t\t\t- {:<32} {}".format("Liquidation price", position.liquidation_price))
 
 
 def main():
@@ -33,11 +62,15 @@ def main():
     parser.add_argument('--print-ping', action='store_true')
     parser.add_argument('--run-examples', action='store_true')
     parser.add_argument('--download-candles', action='store_true')
+    parser.add_argument('--close-positions', action='store_true')
     parser.add_argument('--data-root', action='store', type=str, required=False)
     parser.add_argument('--time-frame', action='store', type=int, required=False, default=60)
     parser.add_argument('--symbol', action='store', type=str, required=False, default='BTC-USDT')
     parser.add_argument('--config-path', action='store', type=str, required=False, default="config.json")
     args = parser.parse_args()
+
+    if args.data_root is not None:
+        os.environ['DATA_ROOT'] = args.data_root
 
     with open(args.config_path, 'r') as file:
         config_data = json.load(file)
@@ -49,7 +82,9 @@ def main():
         run_examples(exchange=exchange, symbol=args.symbol, time_frame=args.time_frame)
     if args.download_candles:
         symbols = config_data['symbols']
-        download_candles(exchange=exchange, symbols=symbols, time_frame=args.time_frame, data_root=args.data_root)
+        download_candles(exchange=exchange, symbols=symbols, time_frame=args.time_frame)
+    if args.close_positions:
+        close_position(exchange=exchange, symbol=args.symbol)
 
 
 if __name__ == '__main__':
