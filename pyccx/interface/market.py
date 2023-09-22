02d1f848fd16b9376de3763802782db60fc9d97e
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2023-09-22 16:29:13 +0330

    add stop_timestamp parameter to get_candles method and fix req_stop value issue

--- pyccx/interface/market.py
+++ pyccx/interface/market.py
@@ -47,8 +47,10 @@ class Market(ABC):
             List[Candle]:
         raise NotImplementedError()
 
-    def get_candles(self, start_timestamp: int, symbol: str, time_frame: TimeFrame, show_tqdm: bool = False):
-        stop_timestamp = datetime.now().timestamp() // time_frame * time_frame
+    def get_candles(self, symbol: str, time_frame: TimeFrame, start_timestamp: int, stop_timestamp: int = None,
+                    show_tqdm: bool = False):
+        if stop_timestamp is None:
+            stop_timestamp = datetime.now().timestamp() // time_frame * time_frame + time_frame
         candles_count = (stop_timestamp - start_timestamp) // time_frame
         requests_count = math.ceil(candles_count / self.max_candles)
 
@@ -59,15 +61,18 @@ class Market(ABC):
 
         candles: List[Candle] = []
         for index in bar:
+            # assign value to req_start as request start timestamp
             if 0 == len(candles):
                 req_start = start_timestamp + index * self.max_candles * time_frame
-                req_stop = req_start + self.max_candles * time_frame
             else:
                 req_start = candles[-1].timestamp + time_frame
-                req_stop = req_start + self.max_candles * time_frame
 
-            req_candles = self.get_historical_candles(symbol=symbol, time_frame=time_frame,
-                                                      start_timestamp=req_start, stop_timestamp=req_stop)
+            # assign value to req_stop as request stop timestamp
+            req_stop = req_start + self.max_candles * time_frame
+            if stop_timestamp < req_stop:
+                req_stop = stop_timestamp
+
+            req_candles = self.get_historical_candles(symbol, time_frame, req_start, req_stop)
             candles.extend(req_candles)
 
         return candles
