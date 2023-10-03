679df6776b6775327a18997874e15b9470ece321
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2023-10-03 21:24:14 +0330

    Update pyccx/exchange/bitget/market.py: rename get_candles to get_recent_candles

--- pyccx/exchange/bitget/future/market.py
+++ pyccx/exchange/bitget/future/market.py
@@ -32,7 +32,7 @@ class BitgetFutureMarket(Market):
         return symbols_info
 
     @symbol_decorator
-    def get_candles(self, symbol: str, time_frame: TimeFrame) -> List[Candle]:
+    def get_recent_candles(self, symbol: str, time_frame: TimeFrame) -> List[Candle]:
         endpoint = "/api/mix/v1/market/candles"
         stop_timestamp = datetime.now().timestamp() // time_frame * time_frame + time_frame
         start_timestamp = stop_timestamp - self.max_candles * time_frame
