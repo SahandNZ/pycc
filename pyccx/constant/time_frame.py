344c3c6687a235ddf7213ad6997707b757f9b45d
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2023-10-11 21:07:46 +0330

    Update pyccx/constant/time_frame.py: add to_str staticmethod

--- pyccx/constant/time_frame.py
+++ pyccx/constant/time_frame.py
@@ -13,3 +13,20 @@ class TimeFrame:
     DAY3 = 3 * DAY1
     WEEK1 = 7 * DAY1
     MONTH1 = 30 * DAY1
+
+    @staticmethod
+    def to_str(value: int) -> str:
+        minute = value // 60
+        hour = minute // 60
+        day = hour // 24
+        week = day // 7
+        if minute < 60:
+            return f"{minute} Minute" + ("s" if 1 < minute else "")
+        elif hour < 24:
+            return f"{hour} Hour" + ("s" if 1 < hour else "")
+        elif day < 7:
+            return f"{day} Day" + ("s" if 1 < day else "")
+        elif week < 4:
+            return f"{week} Week" + ("s" if 1 < week else "")
+        else:
+            return "1 Month"
