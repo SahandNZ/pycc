711a113b59f1864f735e3bc10d7c83bd6696f886
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2023-10-20 18:49:34 +0330

    Update examples/app/app.py: Update examples/app/app.py with changin executor in scheduler

--- examples/app/app.py
+++ examples/app/app.py
@@ -1,27 +1,22 @@
 import argparse
 import json
 import os
+import time
+from datetime import datetime
 
 from pyccx.app.application import Application
 from pyccx.app.context import Context
-from pyccx.constant.time_frame import TimeFrame
 
 
-async def callback(context: Context):
-    for symbol, time_frame in context.pairs:
-        candles = context.live_data.get_candles(symbol, time_frame)
-        print("{:<20} {:<20} {:<20} {:<20}".format(symbol, time_frame, str(candles[-3].datetime), candles[-3].close))
-        print("{:<20} {:<20} {:<20} {:<20}".format(symbol, time_frame, str(candles[-2].datetime), candles[-2].close))
-        print("{:<20} {:<20} {:<20} {:<20}".format(symbol, time_frame, str(candles[-1].datetime), candles[-1].close))
-        print()
-    print("=" * 100)
+def callback(context: Context, sleep: int):
+    print("callback called at {}".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+    time.sleep(sleep)
+    print("callback done   at {}".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data-root', action='store', type=str, required=False, default='./../data/')
-    parser.add_argument('--symbol', action='store', type=str, required=False, default='BTC-USDT')
-    parser.add_argument('--time-frame', action='store', type=int, required=False, default=TimeFrame.HOUR1)
     parser.add_argument('--config-path', action='store', type=str, required=False, default="config.json")
     args = parser.parse_args()
 
@@ -32,8 +27,8 @@ def main():
         config_data = json.load(file)
 
     app = Application.from_config(config_data['pyccx'])
-    app.job_queue.run_once(callback=callback)
-    app.job_queue.run_repeating(callback=callback, interval=60, when='open', misfire_grace_time=30)
+    app.job_queue.run_repeating(callback=callback, kwargs={"sleep": 5}, interval=10, when='open', misfire_grace_time=2)
+    app.job_queue.run_repeating(callback=callback, kwargs={"sleep": 2}, interval=10, when='open', misfire_grace_time=2)
     app.run()
 
 
