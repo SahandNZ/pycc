d84d51237b75538c43d162b0764c7348ac810979
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2023-11-22 14:46:49 +0330

    Update ws: remove ws.py files from interface and exchanges

--- pyccx/exchange/binance/future/ws.py
+++
@@ -1,6 +0,0 @@
-from pyccx.interface.ws import WsClient
-
-
-class BinanceFutureWsClient(WsClient):
-    def __init__(self, key: str = None, secret_key: str = None):
-        super().__init__(base_url="", key=key, secret_key=secret_key)
--- pyccx/interface/ws.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC
-
-
-class WsClient(ABC):
-    def __init__(self, base_url: str, key: str, secret_key: str):
-        self._base_url: str = base_url
-        self._key: str = key
-        self._secret_key: str = secret_key
