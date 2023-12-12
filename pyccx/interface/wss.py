68f092b72e888f6331391bd7130e845a5f3141d2
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2023-12-12 16:14:00 +0330

    Update pyccx/interface/wss.py: Fix 'KeyError: 'socks5'' issue

--- pyccx/interface/wss.py
+++ pyccx/interface/wss.py
@@ -8,7 +8,7 @@ from pyccx.utils.ws import Websocket
 class WssClient(Client):
     def __init__(self, base_url: str, key: str, secret_key: str, proxies: Dict[str, str]):
         super().__init__(base_url, key, secret_key, proxies)
-        self.__ws: Websocket = Websocket(url=base_url, socks5=proxies["socks5"], on_message=self._on_message)
+        self.__ws: Websocket = Websocket(url=base_url, on_message=self._on_message, socks5=proxies.get("socks5", None))
 
     def _send(self, payload: str):
         self.__ws.send(payload)
