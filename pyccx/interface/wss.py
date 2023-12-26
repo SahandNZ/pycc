47f4c033e83521b48684dea24001ebe31ac50291
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2023-12-26 03:41:23 +0330

    Update pyccx/interface/wss.py: Add stream counts limit to create new connections if needed

--- pyccx/interface/wss.py
+++ pyccx/interface/wss.py
@@ -8,10 +8,17 @@ from pyccx.utils.ws import Websocket
 class WssClient(Client):
     def __init__(self, base_url: str, key: str, secret_key: str, proxies: Dict[str, str]):
         super().__init__(base_url, key, secret_key, proxies)
-        self.__ws: Websocket = Websocket(url=base_url, on_message=self._on_message, socks5=proxies.get("socks5", None))
+        self.__socks5 = proxies.get("socks5", None)
+        self.__last_ws: Websocket = None
+        self.__last_ws_streams_count: int = None
 
     def _send(self, payload: str):
-        self.__ws.send(payload)
+        if self.__last_ws is None or 20 < self.__last_ws_streams_count:
+            self.__last_ws: Websocket = Websocket(url=self._base_url, on_message=self._on_message, socks5=self.__socks5)
+            self.__last_ws_streams_count = 0
+
+        self.__last_ws.send(payload)
+        self.__last_ws_streams_count += 1
 
     @abstractmethod
     def _on_message(self, message: str):
@@ -22,4 +29,4 @@ class WssClient(Client):
         raise NotImplementedError()
 
     def join(self):
-        self.__ws.join()
+        self.__last_ws.join()
