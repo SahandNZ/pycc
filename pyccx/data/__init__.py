4a172155c9d91dc26ef898ef812654b804c335e0
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2024-01-13 19:47:15 +0330

    Update pyccx/data/__init__.py: Add new methods get_exchange_symbols, and get_local_symbols

--- pyccx/data/__init__.py
+++ pyccx/data/__init__.py
@@ -1,11 +1,13 @@
 import itertools
+import os
+from typing import Dict, List, Tuple
+
 import pandas as pd
 from rich.progress import Progress
-from typing import Dict, List, Tuple
 
 from pyccx.constant.time_frame import TimeFrame
 from pyccx.data.local import LocalData
-from pyccx.defaults import BASE_TIME_FRAME, PROXIES
+from pyccx.defaults import BASE_TIME_FRAME, PROXIES, DATA_DIR
 from pyccx.interface.exchange import Exchange
 from pyccx.model.candle import Candle
 from pyccx.utils import resample_time_frame
@@ -44,18 +46,13 @@ def load_dataframes_dict(symbols: List[str], time_frames: List[TimeFrame], updat
     return dfs_dict
 
 
-def load_all_dataframes_dict(time_frames: List[TimeFrame], update: bool = False, progress: Progress = None) \
-        -> Dict[Tuple[str, int], pd.DataFrame]:
+def get_local_symbols() -> List[str]:
+    root_dir = os.path.join(DATA_DIR, "candle")
+    symbols = os.listdir(root_dir)
+    return symbols
+
+
+def get_exchange_symbols() -> List[str]:
     symbols_info = __EXCHANGE.future.market.get_symbols_info()
     symbols = [symbol_info.symbol for symbol_info in symbols_info]
-    items = list(itertools.product(symbols, time_frames))
-
-    dfs_dict = {}
-    for symbol, time_frame in items:
-        df = load_dataframe(symbol=symbol, time_frame=time_frame, update=update)
-        dfs_dict[symbol, time_frame] = df
-
-        if progress is not None:
-            progress.update(task, advance=1)
-
-    return dfs_dict
+    return symbols
