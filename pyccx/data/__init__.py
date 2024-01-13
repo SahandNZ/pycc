4f9d7977f47474a494208faed970f9515e09ed0e
Sahand Nazarzadeh <sahand.nazarzadeh@gmail.com>
2024-01-13 19:39:10 +0330

    Update pyccx/defaults.py, and pyccx/data/__init__.py: Add default proxies to defaults.py, And create new load_all_dataframes_dict method

--- pyccx/data/__init__.py
+++ pyccx/data/__init__.py
@@ -1,27 +1,20 @@
 import itertools
+import pandas as pd
+from rich.progress import Progress
 from typing import Dict, List, Tuple
 
-import pandas as pd
 from pyccx.constant.time_frame import TimeFrame
 from pyccx.data.local import LocalData
 from pyccx.defaults import BASE_TIME_FRAME
 from pyccx.interface.exchange import Exchange
 from pyccx.model.candle import Candle
 from pyccx.utils import resample_time_frame
-from rich.progress import Progress
 
-__EXCHANGE: Exchange = None
-__LOCAL_DATA: LocalData = None
+__EXCHANGE: Exchange = Exchange(exchange="binance", proxies=PROXIES)
+__LOCAL_DATA: LocalData = LocalData(exchange=__EXCHANGE)
 
 
-def load_dataframe(exchange: str, symbol: str, time_frame: TimeFrame, update: bool = False,
-                   proxies: Dict[str, str] = None) -> pd.DataFrame:
-    global __EXCHANGE
-    global __LOCAL_DATA
-    if __EXCHANGE is None or __EXCHANGE.exchange != exchange:
-        __EXCHANGE = Exchange(exchange=exchange, proxies=proxies)
-        __LOCAL_DATA = LocalData(exchange=__EXCHANGE)
-
+def load_dataframe(symbol: str, time_frame: TimeFrame, update: bool = False) -> pd.DataFrame:
     if update:
         candles = __LOCAL_DATA.download_candles(symbol=symbol, time_frame=BASE_TIME_FRAME)
         sdf = Candle.to_dataframe(candles=candles)
@@ -33,14 +26,8 @@ def load_dataframe(exchange: str, symbol: str, time_frame: TimeFrame, update: bo
     return df
 
 
-def load_dataframes_dict(
-        exchange: str,
-        symbols: List[str],
-        time_frames: List[TimeFrame],
-        update: bool = False,
-        proxies: Dict[str, str] = None,
-        progress: Progress = None,
-) -> Dict[Tuple[str, int], pd.DataFrame]:
+def load_dataframes_dict(symbols: List[str], time_frames: List[TimeFrame], update: bool = False,
+                         progress: Progress = None) -> Dict[Tuple[str, int], pd.DataFrame]:
     items = list(itertools.product(symbols, time_frames))
 
     if progress is not None:
@@ -48,7 +35,24 @@ def load_dataframes_dict(
 
     dfs_dict = {}
     for symbol, time_frame in items:
-        df = load_dataframe(exchange=exchange, symbol=symbol, time_frame=time_frame, update=update, proxies=proxies)
+        df = load_dataframe(symbol=symbol, time_frame=time_frame, update=update)
+        dfs_dict[symbol, time_frame] = df
+
+        if progress is not None:
+            progress.update(task, advance=1)
+
+    return dfs_dict
+
+
+def load_all_dataframes_dict(time_frames: List[TimeFrame], update: bool = False, progress: Progress = None) \
+        -> Dict[Tuple[str, int], pd.DataFrame]:
+    symbols_info = __EXCHANGE.future.market.get_symbols_info()
+    symbols = [symbol_info.symbol for symbol_info in symbols_info]
+    items = list(itertools.product(symbols, time_frames))
+
+    dfs_dict = {}
+    for symbol, time_frame in items:
+        df = load_dataframe(symbol=symbol, time_frame=time_frame, update=update)
         dfs_dict[symbol, time_frame] = df
 
         if progress is not None:
--- pyccx/defaults.py
+++ pyccx/defaults.py
@@ -7,6 +7,12 @@ from rich.progress import TextColumn, BarColumn, TaskProgressColumn, TimeRemaini
 HOME_DIR = os.path.expanduser('~')
 DATA_DIR = os.environ.get("DATA_DIR", os.path.join(HOME_DIR, "Data"))
 
+# proxies
+HTTP_PROXY = os.environ.get("PYCCX_HTTP_PROXY", None)
+HTTPS_PROXY = os.environ.get("PYCCX_HTTPS_PROXY", None)
+SOCKS5_PROXY = os.environ.get("PYCCX_SOCKS_PROXY", None)
+PROXIES = {"http": HTTP_PROXY, "https": HTTPS_PROXY, "socks5": SOCKS5_PROXY}
+
 # Properties
 BASE_TIME_FRAME = os.environ.get("BASE_TIME_FRAME", 900)
 
